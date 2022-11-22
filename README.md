bc1699e69764c7fda1aa7a118e35c72d70573afa
Sideblade <anhmnguyen2002@gmail.com>
2022-11-21 23:52:08 -0800

    Finished Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 8 - Starter
+1. #1 because a Github action that runs every time code is pushed is the exact scenario that automated testing thrives in. If testing is essential whenever code is changed, it is easiest to have it automated in a github action that will mean code is tested and any errors can be caught as soon as code is pushed. This leads to better workflow that standardizes tests and eliminates the need to indvidually test or only test once development is complete.
+2. No. Function testing is Unit testint, E2E testing deals with the scope of the entire program.
+3. End to End testing would be better to test the entire functionality of the messaging application rather than one function
+4. yes, as a unit test is used to isloate a feature or function of a program and is important when you specifically want to test this function.
+5. ![screenshot 1](https://i.gyazo.com/f88abb13176565b0ecd6a77c78a301e0.png)
+6. ![screenshot 2](https://i.gyazo.com/9e45563c95644ffe4d9a36559b44f9cf.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,69 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('Check isPhoneNumber', () => {
+    expect(functions.isPhoneNumber("(858)375-3852")).toBe(true);
+});
+test('Check isPhoneNumber', () => {
+  expect(functions.isPhoneNumber("8583753852")).toBe(true);
+});
+test('Check isPhoneNumber', () => {
+    expect(functions.isPhoneNumber()).toBe(true);
+});
+test('Check isPhoneNumber', () => {
+    expect(functions.isPhoneNumber("(adw)-231-2134")).toBe(true);
+});
+
+test('Check isEmail', () => {
+    expect(functions.isEmail("dajdwkl@gmail.com")).toBe(true);
+});
+test('Check isEmail', () => {
+    expect(functions.isEmail()).toBe(true);
+});
+test('Check isEmail', () => {
+    expect(functions.isEmail("dajdwklgmail.com")).toBe(true);
+});
+test('Check isEmail', () => {
+    expect(functions.isEmail("dajdwkl@ahgahwa.net")).toBe(true);
+});
+
+test('Check isStrongPassword', () => {
+    expect(functions.isStrongPassword("dad242424D")).toBe(true);
+});
+test('Check isStrongPassword', () => {
+    expect(functions.isStrongPassword("DWADWADWADAWDWDAWDWA")).toBe(true);
+});
+test('Check isStrongPassword', () => {
+    expect(functions.isStrongPassword("413adwadwad")).toBe(true);
+});
+test('Check isStrongPassword', () => {
+    expect(functions.isStrongPassword("DWDAW")).toBe(true);
+});
+
+test('Check isDate', () => {
+    expect(functions.isDate("11/21/2022")).toBe(true);
+});
+test('Check isDate', () => {
+    expect(functions.isDate()).toBe(true);
+});
+test('Check isDate', () => {
+    expect(functions.isDate("11/212022")).toBe(true);
+});
+test('Check isDate', () => {
+    expect(functions.isDate("1/1/2222")).toBe(true);
+});
+
+test('Check isHexColor', () => {
+    expect(functions.isHexColor("#ffffff")).toBe(true);
+});
+test('Check isHexColor', () => {
+    expect(functions.isHexColor()).toBe(true);
+});
+test('Check isHexColor', () => {
+    expect(functions.isHexColor("#fffffj")).toBe(true);
+});
+test('Check isHexColor', () => {
+    expect(functions.isHexColor("#000")).toBe(true);
+});
+
+  
